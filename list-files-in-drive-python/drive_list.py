a41ec2c24add30eb772a3e154d7d7a9bdfff055e
ashishkharche <thekharche@gmail.com>
2022-09-24 00:42:21 +0100

    fix scope gdrive

--- list-files-in-drive-python/drive_list.py
+++ list-files-in-drive-python/drive_list.py
@@ -4,7 +4,7 @@ from googleapiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-SCOPES = 'https://www.googleapis.com/auth/drive.readonly.metadata'
+SCOPES = 'https://www.googleapis.com/auth/drive'
 store = file.Storage('storage.json')
 creds = store.get()
 if not creds or creds.invalid:
